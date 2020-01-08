3b1531da8db8fb79a6fa7d31656cad65ee531760
neigegada <1184575830@qq.com>
2020-01-08 22:31:37 +0800

    添加了修改

--- bksystem/src/main.js
+++ bksystem/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+/**引入elementUI */
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import axios from "axios";
+import router from "./router";
+import store from "./store";
+
+
+Vue.use(ElementUI);
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
